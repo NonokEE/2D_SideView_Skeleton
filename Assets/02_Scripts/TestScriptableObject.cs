9f1252f6e243506a70b5c4eb6ff5915ea94176f5
NonokEE <shshrdl@naver.com>
2025-07-23 23:51:27 +0900

    WIP : 플레이어 이동

--- Assets/02_Scripts/TestScriptableObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "TestScriptableObject", menuName = "Scriptable Objects/TestScriptableObject")]
-public class TestScriptableObject : ScriptableObject
-{
-    
-}
